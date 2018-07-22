6550980fff5967a472a08f47fbe116dcc2219d89
Harry Roshan <harryroshan1997@gmail.com>
2018-07-22 23:29:18 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,45 @@
+var express=require('express');
+var app=express();
+
+var server=require('http').createServer(app);
+var io=require('socket.io').listen(server);
+
+users=[];
+connections=[];
+
+server.listen(process.env.PORT || 3000);
+console.log("Server is running...");
+
+app.get('/', function(req,res){
+	res.sendFile(__dirname+'/index.html');
+})
+
+io.sockets.on('connection', function(socket){
+	connections.push(socket);
+	console.log("A Socket was Connected: %s sockets connected",connections.length);
+
+	socket.on('disconnect',function(data){
+		// if(!socket.username)	return;
+		io.sockets.emit('user exit', {user:socket.username});
+		users.splice(users.indexOf(socket.username), 1);
+		updateUsernames();
+		connections.splice(connections.indexOf(socket),1);
+		console.log("A Socket was Disconnected: %s sockets connected",connections.length);
+	});
+
+	socket.on('send message',function(data){
+		io.sockets.emit('new message', {msg:data,user:socket.username});
+	})
+
+	socket.on('new user',function(data,callback){
+		callback(true);
+		socket.username=data;
+		users.push(socket.username);
+		updateUsernames();
+		io.sockets.emit('welcome user', {user:socket.username});
+	});
+
+	function updateUsernames(){
+		io.sockets.emit('get users',users);
+	}
+});
